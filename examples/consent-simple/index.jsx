8ad376c9ab13597a347094dd2e1cc061cb01dab5
Torgeir Hovden <torgeir@urbanexpression.net>
2021-07-12 13:59:28 +0200

    Add Data Processing Group example

--- examples/consent-simple/index.jsx
+++ examples/consent-simple/index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import React, {useState} from 'react'
 import {SignatuProvider} from '@signatu/sdk'
-import {ConsentCheckbox} from '@signatu/consent-react';
+import {ConsentCheckbox, DataProcessingGroup} from '@signatu/consent-react';
 
 const consentTarget = {
     /** The ID of the subject */
@@ -13,16 +13,26 @@ const consentTarget = {
 
 const ConsentSimple = () =>  {
     const [accessToken, setAccessToken] = useState()
+    const [dpGroupId, setDpGroupId] = useState()
     return (
         <>
+            <h1>API Access Token</h1>
             <input onChange={ev => setAccessToken(ev.target.value)} placeholder="signatu access token"/>
             <SignatuProvider accessToken={accessToken}>
+                <h1>Simple Checkbox</h1>
                 <ConsentCheckbox
                     consentTarget={consentTarget}
                     language="en"
                 >
                     You can embed whatever content you want here
                 </ConsentCheckbox>  
+
+                {/* An example using a more advanced control for a Signatu Data Processing Group */}
+                <h1>Data Processing Group</h1>
+                <div>Make sure the Access Token has <code>dataprocessing:read</code> or <code>dataprocessing</code> scope permissions. </div>
+                <br/>
+                <input style={{minWidth: 400}} placeholder="Input a Data Processing Group ID to autogenerate consent controls" onChange={(ev) => setDpGroupId(ev.target.value)} value={dpGroupId} />
+                <DataProcessingGroup groupId={dpGroupId} />
             </SignatuProvider>
 
         </>
